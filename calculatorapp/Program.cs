09adac5fe525651a1b9c904f5b62732a45f9285a
zarifatb-15 <zarifatb@code.edu.az>
2026-02-22 23:52:10 +0400

    added number operation

--- calculatorapp/Program.cs
+++ calculatorapp/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcome to the Calculator App!");
+        Console.Write("Please enter the first number: ");
+        double num1= Convert.ToDouble(Console.ReadLine());
+        
+        Console.Write("Please choice +, -, *, or /: ");
+        char op=Console.ReadKey().KeyChar;
+        Console.WriteLine();
+        
+        Console.Write("Please enter the second number: ");
+        double num2=Convert.ToDouble(Console.ReadLine());
+
+        double result = 0;
+        bool validOperation = true;
+
+
     }
 }
